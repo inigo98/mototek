73a42cacdeb436ce83db4ec7c4072adf20ae01f4
dilan RM <dilan_ceti@live.com.mx>
2021-06-29 00:26:56 -0500

    29/06/2021

--- ClientApp/src/app/detalle-compra/detalle-compra.component.ts
+++ ClientApp/src/app/detalle-compra/detalle-compra.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ApiUrlService } from '../api-url.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-detalle-compra',
@@ -7,7 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalleCompraComponent implements OnInit {
 
-  constructor() { }
+  transferencia: any;
+
+  url = '';
+  constructor(public http: HttpClient, public apiUrl: ApiUrlService, private router: Router) {
+
+    this.http.get(this.url + 'historial').subscribe(data => {
+      console.log(data);
+      this.transferencia = data['data'];
+    })
+
+  }
 
   ngOnInit() {
   }
